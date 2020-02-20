656f8d34558062d43d4940201af8b967dd98c621
Adrian Novegil <adrian.novegil@gmail.com>
2020-02-20 22:09:46 +0100

    Updating the load server

--- docker/load-server/docker-entrypoint.sh
+++ docker/load-server/docker-entrypoint.sh
@@ -2,8 +2,10 @@
 set -e
 
 until $(curl --output /dev/null --silent --head --fail http://api-gateway:8080); do
-  printf '.\n'
+  printf 'waiting for API Gateway ...\n'
   sleep 10
 done
 
-exec artillery "$@"
+sleep 30
+
+while true; do artillery "$@"; done
